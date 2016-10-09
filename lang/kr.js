68e08ff508b4e050508cad4dd4db1ae439f0c3d6
Castar <Castar@users.noreply.github.com>
2016-10-10 06:17:03 +0900

    Update kr.js

--- lang/kr.js
+++ lang/kr.js
@@ -1,3 +1,27 @@
 module.exports = {
-	lang: "한국어"
+	lang: "한국어",
+	uncategorized: {
+		search: "검색...",
+		leaderboard: "랭킹",
+		login: "로그인",
+		logout: "로그아웃",
+		register: "가입하기",
+		performance: "Performance",
+		score: "점수",
+		dashboard: "대시보드",
+	},
+	auth: {
+		login: {
+			name: "아이디",
+			password: "비밀번호",
+
+			forgotpass: "비밀번호를 잊으셨나요?",
+
+			success: "성공적으로 로그인 했습니다.",
+			error: "아이디나 비밀번호가 틀렸습니다. 다시 시도해 주세요."
+		},
+		logout: {
+			success: "성공적으로 로그아웃 되었습니다."
+		}
+	}
 }
