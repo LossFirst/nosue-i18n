d7c71602aac56385313129cb129d7ecbccf83b73
LossFirst <osumusicchannel@gmail.com>
2016-10-10 18:15:25 +0900

    Update jp.js

--- lang/jp.js
+++ lang/jp.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
 	lang: "日本語",
 	common: {
@@ -74,32 +73,4 @@ module.exports = {
 		ranked: "{date} にRanked",
 		download: "ダウンロード",
 	}
-=======
-module.exports = {
-	lang: "日本語",
-	uncategorized: {
-		search: "検索...",
-		leaderboard: "ランキング",
-		login: "ログイン",
-		logout: "ログアウト",
-		register: "登録",
-		performance: "パフォーマンス",
-		score: "スコア",
-		dashboard: "ダッシュボード",
-	},
-	auth: {
-		login: {
-			name: "ユーザー名",
-			password: "パスワード",
-
-			forgotpass: "パスワードを忘れましたか？",
-
-			success: "ログインが完了しました。",
-			error: "ユーザー名かパスワードを確認してください。"
-		},
-		logout: {
-			success: "ログアウトが完了しました。"
-		}
-	}
->>>>>>> origin/master
 }
